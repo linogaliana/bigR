265af0457b2fedf7f1c0faeb42f0cd132a1e0fbf
Lino Galiana <lino.galiana@insee.fr>
2019-07-01 21:11:33 +0200

    Try another strategy to find statistics module

--- build.R
+++ build.R
@@ -1,7 +1,10 @@
 #!/usr/bin/Rscript
 
 print(Sys.which("python"))
-Sys.setenv(RETICULATE_PYTHON = "/opt/conda/bin")
+Sys.setenv(RETICULATE_PYTHON = "/opt/conda/envs/Reticulate")
+reticulate::conda_install("Reticulate", "scipy")
+reticulate::conda_install("Reticulate", "statistics")
+
 print(reticulate::py_config())
 
 f = list.files('supports', 'Rmd$', full.names = TRUE, recursive = TRUE)
